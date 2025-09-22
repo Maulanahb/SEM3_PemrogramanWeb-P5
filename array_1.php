3e8a74d457076b7334cdccb127a3ce441e6f0dc2
Maulanahb <maulanahmad2287@gmail.com>
2025-09-22 20:50:19 +0700

    Add initial HTML structure and PHP code for displaying indexed array

---
+++ array_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+        $Listdosen=["Elok Nur Hamadana","Unggul Pamenang","Bagas Nughraha"];
+        echo $Listdosen[2]."<br/>";
+        echo $Listdosen[0]."<br/>";
+        echo $Listdosen[2]."<br/>";
+    ?>
+</body>
+</html>
