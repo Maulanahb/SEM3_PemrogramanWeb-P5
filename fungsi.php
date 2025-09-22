228835b9b9206274904071ecb8a57bce5894f467
Maulanahb <maulanahmad2287@gmail.com>
2025-09-22 19:54:15 +0700

    first commit

---
+++ fungsi.php
@@ -0,0 +1,11 @@
+<?php
+    function perkenalan(){
+        echo "Assalamualaikum, ";
+        echo "Perkenalkan, nama saya Maulana <br/>";
+        echo "Senang berkenalan dengan Anda <br/>";
+    }
+
+    //memanggil fungsi
+    perkenalan();
+    perkenalan();
+?>
