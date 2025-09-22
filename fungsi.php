1f15aa4481b5044f722db00137f5cd2b2e67d50d
Maulanahb <maulanahmad2287@gmail.com>
2025-09-22 20:28:17 +0700

    Refactor perkenalan function to improve structure and maintainability

--- fungsi.php
+++ fungsi.php
@@ -1,26 +1,18 @@
 <?php
-    function perkenalan($nama ,$salam="Assalamualaikum"){
-        echo $salam.", ";
-        echo "Perkenalkan, nama saya ".$nama."<br/>";
-        echo "Senang berkenalan dengan Anda <br/>";
-    }
-
-    //memanggil fungsi yang sudah dibuat
-    perkenalan("Maulana", "Hallo");
-    
-    echo "<hr>";
-
-    $saya = "Maulana";
-    $ucapanSalam = "Selamat pagi";
-    perkenalan($saya);
-    echo "<hr>";
-    
     //fungsi hitung umur
     function hitungUmur($thn_lahir, $thn_sekarang){
         $umur = $thn_sekarang - $thn_lahir;
         return $umur;
     }
+    function perkenalan($nama ,$salam="Assalamualaikum"){
+        echo $salam.", ";
+        echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-    echo "Umur saya adalah ".hitungUmur(2006, 2025)." tahun<br/>";
+        //memanggil fungsi hitung umur
+        echo "Saya berusia ". hitungUmur(2006, 2025)." tahun<br/>";
+        echo "Senang berkenalan dengan anda<br/>";
+    }
 
+    //memanggil fungsi perkenalan
+    perkenalan("Maulana" );
 ?>
