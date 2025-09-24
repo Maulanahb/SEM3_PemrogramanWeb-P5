fec207b56d92c79566f62c878de66ff3f7dd2936
Maulanahb <maulanahmad2287@gmail.com>
2025-09-24 18:36:11 +0700

    String karakter melarikan diri

---
+++ string2.php
@@ -0,0 +1,10 @@
+<?php 
+    echo "Baris1baru <br>";   //soal 10.a
+    echo 'Baris2baru <br>';   //soal 10.b
+    echo "Halo\tDunia <br>";  //soal 10.c
+    echo 'Halo\tDunia <br>';  //soal 10.d
+    echo "Rsep\halo\tDunia!<br>"; //soal 10.e
+    echo 'Rsep\halo\tDunia!<br>'; //soal 10.f
+    echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 10.g
+    echo 'Katakanlah \"Tidak pada narkoba!\" <br>'; //soal 10.h
+?>
