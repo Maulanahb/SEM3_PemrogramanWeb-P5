9145952c4f1c4a60fe50b82e9383effd515e0d9d
Maulanahb <maulanahmad2287@gmail.com>
2025-09-24 18:44:47 +0700

    untuk membalikkn kata per kata

--- string3.php
+++ string3.php
@@ -1,4 +1,10 @@
 <?php
 $pesan="Saya arek Malang";
-echo strrev($pesan)."<br/>";
+#ubah variabel pesan menjadi array dengan perintah explode
+$pesanPerKata=explode(" ",$pesan);
+#ubah setiap kata dalam array menjadi terbalik
+$pesanPerKata=array_map(fn($pesan ) => strrev($pesan), $pesanPerKata);
+#gabungkan kembali array menjadi string
+$pesan=implode(" ",$pesanPerKata);
+echo$pesan."<br/>";
 ?>
